71e096c04551a43dded401e9dfe6573083cd6c4a
artur.musayelyan <musayelyan099@gmail.com>
2017-11-01 19:01:37 +0400

    01.11.2017 ArtCommitgorcic ( 3 )

--- app/src/main/java/com/example/arturmusayelyan/slidenerd2/ListViewWithCustomAdapter.java
+++ app/src/main/java/com/example/arturmusayelyan/slidenerd2/ListViewWithCustomAdapter.java
@@ -1,9 +1,18 @@
 package com.example.arturmusayelyan.slidenerd2;
 
+import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class ListViewWithCustomAdapter extends AppCompatActivity {
     private ListView listView;
@@ -23,43 +32,43 @@ public class ListViewWithCustomAdapter extends AppCompatActivity {
 
 //        MyCustomAdapter adapter = new MyCustomAdapter(this, titles, description, images);
 //        listView.setAdapter(adapter);
-        CustomAdapter2 adapter2=new CustomAdapter2(this,titles,description,images);
+        CustomAdapter2 adapter2 = new CustomAdapter2(this, titles, description, images);
         listView.setAdapter(adapter2);
     }
 }
 
-//class MyCustomAdapter extends ArrayAdapter<String> {
-//    Context context;
-//    String[] titleArray;
-//    String[] descriptionArray;
-//    int[] imagesArray;
-//
-//    public MyCustomAdapter(Context context, String[] titles, String[] description, int[] images) {
-//        super(context, R.layout.single_row_for_customadapter, R.id.textView_forCustomAdapter1, titles);
-//        this.context = context;
-//        this.titleArray = titles;
-//        this.descriptionArray = description;
-//        this.imagesArray = images;
-//    }
-//
-//    @NonNull
-//    @Override
-//    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-//        //150% optimization
-//        View row = convertView;
-//        if (row == null) {
-//            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//            row = inflater.inflate(R.layout.single_row_for_customadapter, parent, false);
-//        }
-//        //
-//        TextView myTitle = row.findViewById(R.id.textView_forCustomAdapter1);
-//        TextView myDescription = row.findViewById(R.id.textView_forCustomAdapter2);
-//        ImageView myImage = row.findViewById(R.id.imageView_forCustomAdapter);
-//
-//        myTitle.setText(titleArray[position]);
-//        myDescription.setText(descriptionArray[position]);
-//        myImage.setImageResource(imagesArray[position]);
-//
-//        return row;
-//    }
-//}
+class MyCustomAdapter extends ArrayAdapter<String> {
+    Context context;
+    String[] titleArray;
+    String[] descriptionArray;
+    int[] imagesArray;
+
+    public MyCustomAdapter(Context context, String[] titles, String[] description, int[] images) {
+        super(context, R.layout.single_row_for_customadapter, R.id.textView_forCustomAdapter1, titles);
+        this.context = context;
+        this.titleArray = titles;
+        this.descriptionArray = description;
+        this.imagesArray = images;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        //150% optimization
+        View row = convertView;
+        if (row == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            row = inflater.inflate(R.layout.single_row_for_customadapter, parent, false);
+        }
+        //
+        TextView myTitle = row.findViewById(R.id.textView_forCustomAdapter1);
+        TextView myDescription = row.findViewById(R.id.textView_forCustomAdapter2);
+        ImageView myImage = row.findViewById(R.id.imageView_forCustomAdapter);
+
+        myTitle.setText(titleArray[position]);
+        myDescription.setText(descriptionArray[position]);
+        myImage.setImageResource(imagesArray[position]);
+
+        return row;
+    }
+}
